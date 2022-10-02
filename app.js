07b9196bac55368482f62a783f4f007aa8af1a5a
Pedro González Carretero <73654861+Spiki3@users.noreply.github.com>
2022-10-02 19:52:21 -0300

    Cambios en el header y estructura Hero pagina de inicio
    Estuve haciendo cambios en el inicio, a partir de un boceto que hice en Figma
    
    https://www.figma.com/file/Y01IkUmCdgoBDprw7a2rXS/CULINARIO?node-id=0%3A1
    
    Falta hacer mucho, pero lo subo para mantener esta estructura

--- app.js
+++ app.js
@@ -21,45 +21,125 @@ div.appendChild(aLogo);
 miFragmentoHeader.appendChild(div);
 
 //Nav
-const nav = document.createElement("nav");
+const navHeader = document.createElement("nav");
 const listaU = document.createElement("ul");
-const liQuienes = document.createElement("li");
 const liMenu = document.createElement("li");
 const liReservas = document.createElement("li");
+const liSucursales = document.createElement("li");
 const liContacto = document.createElement("li");
-const aQuienes = document.createElement("a");
 const aMenu = document.createElement("a");
 const aReservas = document.createElement("a");
+const aSucursales = document.createElement("a");
 const aContacto = document.createElement("a");
 
-aQuienes.setAttribute("href", "/views/aboutUs.html");
 aMenu.setAttribute("href", "/views/menu.html");
 aReservas.setAttribute("href", "/views/reservas.html");
+aSucursales.setAttribute("href", "/views/aboutUs.html");
 aContacto.setAttribute("href", "contacto.html");
 
-liQuienes.classList.add("misLinks");
 liMenu.classList.add("misLinks");
 liReservas.classList.add("misLinks");
+liSucursales.classList.add("misLinks");
 liContacto.classList.add("misLinks");
 
-aQuienes.textContent = "Quienes somos";
-aMenu.textContent = "Menu";
-aReservas.textContent = "Reservas";
+aMenu.textContent = "Reservas";
+aReservas.textContent = "Sucursales";
+aSucursales.textContent = "Menú";
 aContacto.textContent = "Contacto";
 
-liQuienes.appendChild(aQuienes);
 liMenu.appendChild(aMenu);
 liReservas.appendChild(aReservas);
+liSucursales.appendChild(aSucursales);
 liContacto.appendChild(aContacto);
 
-listaU.appendChild(liQuienes);
 listaU.appendChild(liMenu);
 listaU.appendChild(liReservas);
+listaU.appendChild(liSucursales);
 listaU.appendChild(liContacto);
 
 
-nav.appendChild(listaU);
+navHeader.appendChild(listaU);
 
-miFragmentoHeader.appendChild(nav);
+miFragmentoHeader.appendChild(navHeader);
 
 miHeader.appendChild(miFragmentoHeader);
+
+miHeader.classList.add("header");
+
+
+
+
+// //Footer Dinamico
+
+// //Nav Interno
+// const navFooter = document.createElement("nav");
+// const miFooter = document.getElementById("miFooterDinamico");
+// const miFragmentoFooter = document.createDocumentFragment();
+
+// const liCuenta = document.createElement("li");
+// const liIngresa = document.createElement("li");
+// const liCompras = document.createElement("li");
+// const aCuenta = document.createElement("a");
+// const aIngresa = document.createElement("a");
+// const aCompras = document.createElement("a");
+
+// aCuenta.setAttribute("href", "#" );
+// aIngresa.setAttribute("href", "#");
+// aCompras.setAttribute("href", "#");
+
+// liCuenta.classList.add("misLinks");
+// liIngresa.classList.add("misLinks");
+// liCompras.classList.add("misLinks");
+
+// aCuenta.textContent = "Crea tu cuenta";
+// aIngresa.textContent = "Ingresar";
+// aCompras.textContent = "Mis Compras";
+
+// liCuenta.appendChild(aCuenta);
+// liIngresa.appendChild(aIngresa);
+// liCompras.appendChild(aCompras);
+
+// listaU.appendChild(liCuenta);
+// listaU.appendChild(liIngresa);
+// listaU.appendChild(liCompras);
+
+// navFooter.appendChild(listaU);
+
+// miFragmentoFooter.appendChild(navFooter);
+
+
+// //Nav Social Links\\
+
+// const liInstagram = document.createElement("li");
+// const liFacebook = document.createElement("li");
+// const liTwitter = document.createElement("li");
+// const aInstagram = document.createElement("a");
+// const aFacebook = document.createElement("a");
+// const aTwitter = document.createElement("a");
+
+// aInstagram.setAttribute("href", "#" );
+// aFacebook.setAttribute("href", "#");
+// aTwitter.setAttribute("href", "#");
+
+// liInstagram.classList.add("logoSocial");
+// liFacebook.classList.add("logoSocial");
+// liTwitter.classList.add("logoSocial");
+
+// aInstagram.textContent = "Crea tu cuenta";
+// aIngresa.textContent = "Ingresar";
+// aCompras.textContent = "Mis Compras";
+
+// liInstagram.appendChild(aCuenta);
+// liIngresa.appendChild(aIngresa);
+// liCompras.appendChild(aCompras);
+
+// listaU.appendChild(liCuenta);
+// listaU.appendChild(liIngresa);
+// listaU.appendChild(liCompras);
+
+// navFooter.appendChild(listaU);
+
+// miFragmentoFooter.appendChild(navFooter);
+
+
+// miFooter.appendChild(miFragmentoFooter);
